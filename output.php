d5af3855bb5a7e8c9563486b3f9308a42c6a2022
badbons <52952112+badbons@users.noreply.github.com>
2019-07-16 14:45:09 +0700

    Add files via upload

---
+++ output.php
@@ -0,0 +1,15 @@
+<?php
+$Nama=$_POST['Nama'];
+$Tempat=$_POST['Tempat'];
+$TLL=$_POST['TTL'];
+$JK=$_POST['JK'];
+$alamat=$_POST['alamat'];
+$usia=$_POST['usia'];
+
+echo "Nama     : $Nama<br>";
+echo "Tempat  : $Tempat<br>";
+echo "Tanggal Lahir : $TLL<br>";
+echo "Jenis Kelamin : $JK<br>";
+echo "Alamat : $alamat<br>";
+echo "Usia : $usia<br>";
+?>
